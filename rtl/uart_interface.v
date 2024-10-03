050a4107d38d0372219453e564f5fbb8b3dffbe1
w3rqil <leonel.mansilla@mi.unc.edu.ar>
2024-10-03 11:30:35 -0300

    format update

--- rtl/uart_interface.v
+++ rtl/uart_interface.v
@@ -11,21 +11,21 @@ module uart_interface
     output      wire        [NB_DATA - 1 : 0]   o_data
 );
 
-    reg  [5:0]                  state                             ;
-    reg  [1:0]                  done_counter                      ;
-    reg [NB_OP-1:0] op;
-    reg [NB_DATA-1:0] datoB;
-    reg [NB_DATA-1:0] datoA;
-    reg                     valid;
+    reg [5:0]               state                                           ;
+    reg [1:0]               done_counter                                    ;
+    reg [NB_OP   - 1 : 0]   op                                              ;
+    reg [NB_DATA - 1 : 0]   datoB                                           ;
+    reg [NB_DATA - 1 : 0]   datoA                                           ;
+    reg                     valid                                           ;
     
-    reg  [3:0]                  next_state                       ;
-    reg  [1:0]                  next_done_counter                ;
-    reg  [NB_DATA-1:0]      type_reg                             ;
-    reg                     next_valid;
-    reg [NB_DATA-1:0] next_datoA;
-    reg [NB_DATA-1:0] next_datoB;
-    reg [NB_OP-1:0] next_op;
-    wire  [NB_DATA - 1 : 0] leds_reg;
+    reg  [3:0]              next_state                                      ;
+    reg  [1:0]              next_done_counter                               ;
+    reg  [NB_DATA - 1 : 0]  type_reg                                        ;
+    reg                     next_valid                                      ;
+    reg  [NB_DATA - 1 : 0]  next_datoA                                      ;
+    reg  [NB_DATA - 1 : 0]  next_datoB                                      ;
+    reg  [NB_OP - 1 : 0  ]  next_op                                         ;
+    wire [NB_DATA - 1 : 0]  leds_reg                                        ;
 
 
     localparam [5:0]
@@ -38,19 +38,19 @@ module uart_interface
 
     always @(posedge clk or negedge i_rst_n) begin
         if(!i_rst_n) begin
-            state <= IDLE                                           ;
-            done_counter <= 0                                       ;
-            valid <= 0 ;
-            datoA <= 0;
-            datoB <= 0;
-            op <= 0;
+            state <= IDLE                                                   ;
+            done_counter <= 0                                               ;
+            valid <= 0                                                      ;
+            datoA <= 0                                                      ;
+            datoB <= 0                                                      ;
+            op <= 0                                                         ;
         end else begin              
-            state <= next_state                                     ;
-            done_counter <= next_done_counter                       ;
-            valid <= next_valid;
-            datoA <= next_datoA ;
-            datoB <= next_datoB;
-            op <= next_op;
+            state <= next_state                                             ;
+            done_counter <= next_done_counter                               ;
+            valid <= next_valid                                             ;
+            datoA <= next_datoA                                             ;
+            datoB <= next_datoB                                             ;
+            op <= next_op                                                   ;
 
         end
     end
@@ -60,67 +60,68 @@ module uart_interface
         case(state)
             IDLE: begin
                 if (i_rxDone) begin 
-                    next_state = PARSER ;
-                    next_done_counter = done_counter + 1;
-                    type_reg = i_rx;
-                    
-                end else begin 
-                    next_state = IDLE;
-                    next_done_counter = 0 ;
+                    next_state = PARSER                                     ;
+                    next_done_counter = done_counter + 1                    ;
+                    type_reg = i_rx                                         ;
+
+                end else begin                  
+                    next_state = IDLE                                       ;
+                    next_done_counter = 0                                   ;
                 end
                 
             end
             PARSER: begin
-                next_valid = 0;
-                if(i_rxDone) begin
-                    case(type_reg)
-                        DATOA: begin
-                            next_datoA = i_rx;
-                        end
-                        DATOB: begin
-                            next_datoB = i_rx;
-                        end
-                        OP: begin
-                            next_op = i_rx;
-                            next_valid = 1;
+                next_valid = 0                                              ;
+                if(i_rxDone) begin                          
+                    case(type_reg)                          
+                        DATOA: begin                            
+                            next_datoA = i_rx                               ;
+                        end                         
+                        DATOB: begin                            
+                            next_datoB = i_rx                               ;
+                        end                         
+                        OP: begin                           
+                            next_op = i_rx                                  ;
+                            next_valid = 1                                  ;
                         end
                     endcase
-                    next_done_counter = done_counter + 1;
+                    next_done_counter = done_counter + 1                    ;
                 end
-                next_state = (done_counter == 2) ? STOP : PARSER;  
+                next_state = (done_counter == 2) ? STOP : PARSER            ;  
             end
             STOP: begin
-                next_state <= IDLE                                           ;
-                done_counter <= 0                                       ;
-                valid <= 0 ;
-                datoA <= 0;
-                datoB <= 0;
-                op <= 0;                
+                next_state <= IDLE                                          ;
+                done_counter <= 0                                           ;
+                valid <= 0                                                  ;
+                datoA <= 0                                                  ;
+                datoB <= 0                                                  ;
+                op <= 0                                                     ;                
             end
             default: begin
-                next_datoA =   next_datoA;
-                next_datoB = next_datoB;
-                next_op = next_op;
-                next_state = next_state;
-                next_valid = next_valid;
-                next_done_counter = next_done_counter;
+                next_datoA =   next_datoA                                   ;
+                next_datoB = next_datoB                                     ;
+                next_op = next_op                                           ;
+                next_state = next_state                                     ;
+                next_valid = next_valid                                     ;
+                next_done_counter = next_done_counter                       ;
                 
             end
         endcase
     end
     assign o_data = leds_reg;
+    
     alu
     #(
-        .NB_DATA(NB_DATA),
-        .NB_OP(NB_OP)
+        .NB_DATA        (NB_DATA    ),
+        .NB_OP          (NB_OP      )
     )
     u_alu
     (
-        .i_valid(valid),
-        .i_datoA(datoA),
-        .i_datoB(datoB),
-        .i_operation(operation),
-        .o_leds(leds_reg)
+        .i_valid        (valid      ),
+        .i_datoA        (datoA      ),
+        .i_datoB        (datoB      ),
+        .i_operation    (operation  ),
+        .o_leds         (leds_reg   )
     );
 
 endmodule
